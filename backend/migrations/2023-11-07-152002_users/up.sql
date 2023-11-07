387de7a16aed3d4d1741984a980df6192d885259
alberto <aarosteguig@gmail.com>
2023-11-07 19:48:04 +0100

    Avance según REST API con ORM diesel

---
+++ backend/migrations/2023-11-07-152002_users/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE USERS (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    first_name TEXT NOT NULL
+)
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,38 +1,55 @@
-use bdk::{SyncOptions, descriptor};
-use bdk::blockchain::ElectrumBlockchain;
-use bdk::electrum_client::Client;
-use bdk::{Wallet, database::MemoryDatabase};
-use bdk::bitcoin::Network;
-use dotenv::{self, from_filename, dotenv};
-use std::env::var;
+#![feature(decl_macro, proc_macro_hygiene)]
+#![allow(proc_macro_derive_resolution_fallback, unused_attributes)]
 
-fn main() -> anyhow::Result<()> {
-    from_filename(".env").ok();
-    dotenv::dotenv().ok();
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
-    let descriptor = var("WALLET_DESCRIPTOR").unwrap();
-    dbg!(&descriptor);
+use dotenv::dotenv;
+use std::env;
+use routes::*;
+use std::process::Command;
 
-    let wallet = Wallet::new(&descriptor, None, Network::Testnet, MemoryDatabase::default())?;
-    let client = Client::new("ssl://electrum.blockstream.info:60002")?;
-    let blockchain = ElectrumBlockchain::from(client);
+mod db;
+mod models;
+mod routes;
+mod schema;
 
-    wallet.sync(&blockchain, SyncOptions::default());
-    let balance = wallet.get_balance();
-    let address = wallet.get_address(bdk::wallet::AddressIndex::New);
+fn rocket() -> rocket::Rocket {
+    dotenv().ok();
 
-    dbg!(balance);
-    dbg!(address);
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+
+    let pool = db::init_pool(database_url);
+    rocket::ignite()
+        .manage(pool)
+        .mount(
+            "/api/v1/",
+            routes![get_all, new_user, find_user],
+        )
 }
 
-fn setup() -> String {
-    from_filename(".env");
-    dotenv();
-
-    let descriptor = var("WALLET_DESCRIPTOR");
-
-    match descriptor {
-        Ok(descriptor) => descriptor,
-        Err(_) => "Couldn't fetch WALLET_DESCRIPTOR env variable".to_string()
-    }
+fn main() {
+    let _output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "cd ui && npm start"])
+            .spawn()
+            .expect("Failed to start UI Application")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("cd ui && npm start")
+            .spawn()
+            .expect("Failed to start UI Application")
+    };
+    rocket().launch();
 }
