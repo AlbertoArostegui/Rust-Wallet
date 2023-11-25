141920437a37ccdedd7fcf1363306bc80fc9bc3c
Alberto <aarosteguig@gmail.com>
2023-11-25 01:47:15 +0100

    Funcional completamente

--- frontend/src/components/Mypage.vue
+++ frontend/src/components/Mypage.vue
@@ -7,7 +7,10 @@ export default {
   data () {
     return {
       balance: '0.0',
-      address: 'loading...'
+      address: 'loading...',
+      hash_error: '',
+      address_to: '',
+      amount: ''
     }
   }, 
   methods: {
@@ -32,6 +35,28 @@ export default {
       const response_json = await response.json();
       return response_json;
     },
+    async sendTransaction() {
+      console.log('sendTransaction() triggered in Mypage.vue');
+      if (this.address_to.length < 42) {
+        this.hash_error = 'Value must be a valid ETH address';
+        return false;
+      }
+      let parsed_amout = parseFloat(this.amount);
+      const response = await fetch('/api/sendTransaction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length':  '',
+        },
+        body: JSON.stringify({
+          email: this.$store.state.user,
+          to: this.address_to,
+          amount: parsed_amout
+        }),
+      });
+      const response_json = await response.json();
+      return response_json;
+    }
   },
   async created() {
     
@@ -50,7 +75,20 @@ export default {
       <h2>{{ balance }} Ethereum on {{ address }}</h2>
     </div>
     <div id="transaction-zone">
-      <input type="text">
+      <form class="form" @submit.prevent="sendTransaction">
+        <div class="form-group">
+          <label for="address_to" class="form-label">Address to send it to</label>
+          <input type="text" id="address_to" class="form-input" v-model="address_to"/>
+          <p v-if="hash_error" class="error-text">{{ hash_error }}</p>
+        </div>
+        <div class="form-group">
+          <label for="amount" class="form-label">Amount in ETH to send (Fees not included)</label>
+          <input type="text" id="amount" class="form-input" v-model="amount"/>
+        </div>
+        <div id="submit-button">
+      <button type="submit" class="btn btn-primary">Send</button>
+    </div>
+      </form>
     </div>
 </body>
 </template>
@@ -61,10 +99,6 @@ export default {
         align-self: center;
     }
 
-    a {
-        text-decoration: none;
-    }
-
     .texto {
         padding: 5rem;
     }
@@ -73,6 +107,47 @@ export default {
         padding: 5rem;
     }
 
+    .form {
+        padding-top: 3rem;
+        max-width: 400px;
+        margin: 0 auto;
+        color: rgb(200, 192, 147);
+    }
+
+    .form-group {
+        margin-bottom: 1rem;
+    }
+
+    .form-label {
+      display: block;
+      margin-bottom: 0.5rem;
+      text-align: left;
+      color: var(--color-text);
+    }
+
+    .form-input {
+      display: block;
+      width: 100%;
+      text-align: left;
+      padding: 0.5rem;
+      font-size: 1rem;
+      line-height: 1.5;
+      color: var(--color-text);
+      background-color: var(--color-background);
+      border: 1px solid var(--color-border);
+      border-radius: 0.25rem;
+      transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    }
+
+    .error-text {
+      color: red;
+    }
+
+    .form-input:focus {
+      outline: none;
+      border-color: var(--color-primary);
+      box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.25);
+    }
     img {
         width: 50vw;
     }
@@ -83,8 +158,63 @@ export default {
     }
 
     #portfolio {
-        background-color: darkslateblue;
         padding-left: 3rem;
         border-radius: 12px;
+    }
+
+    #transaction-zone {
+      padding-top: 3rem;
+    }
+
+    .btn {
+      display: inline-block;
+      font-weight: 400;
+      color: var(--color-button-text);
+      text-align: center;
+      vertical-align: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      background-color: var(--color-button-background);
+      border: 1px solid var(--color-button-border);
+      padding: 0.375rem 0.75rem;
+      font-size: 1rem;
+      line-height: 1.5;
+      border-radius: 0.25rem;
+      transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+        border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    }
+
+    .btn:hover {
+      color: var(--color-button-text-hover);
+      background-color: var(--color-button-background-hover);
+      border-color: var(--color-button-border-hover);
+    }
+
+    .btn:focus {
+      outline: none;
+      box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.25);
+    }
+
+    .btn-primary {
+      color: var(--color-primary-text);
+      background-color: var(--color-primary);
+      border-color: var(--color-primary);
+    }
+
+    .btn-primary:hover {
+      color: var(--color-primary-text-hover);
+      background-color: var(--color-primary-hover);
+      border-color: var(--color-primary-hover);
+    }
+
+    .btn-primary:focus {
+      outline: none;
+      box-shadow: 0 0 0 0.2rem rgba(200, 192, 147, 100);
+    }
+
+    #submit-button {
+      text-align: center;
     }
 </style>
